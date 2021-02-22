d6c4c9cb75baac6092857befc1077945b559c194
Leon Michalak <leonmichalak6@gmail.com>
2021-02-22 15:42:39 +0000

    File downloads

--- controllers/download.js
+++ controllers/download.js
@@ -1,7 +1,27 @@
-function post_download(req, res, next) {
-  return res.status(200).json({ message: "Downloaded successfully", errors: false });
+const path = require("path");
+
+const dir = path.dirname(require.main.filename);
+
+function get_download(req, res, next) {
+  const filename = req.params.filename;
+
+  const file_path = path.join(dir, "files", filename);
+
+  res.download(file_path, "CV.pdf", function (err) {
+    if (err) {
+      // Handle error, but keep in mind the response may be partially-sent
+      // so check res.headersSent
+      if (res.headersSent) {
+        return console.log(res.headersSent);
+      } else {
+        return res.status(500).json({ message: "Downloaded unsuccessfully", errors: err });
+      }
+    } else {
+      return res.status(200).json({ message: `Downloaded ${filename} successfully`, errors: false });
+    }
+  });
 }
 
 module.exports = {
-  post_download,
+  get_download,
 };
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require("express");
-require("dotenv").config();
 const cors = require("cors");
+require("dotenv").config();
 
-const { post_download } = require("./controllers/download");
+const { get_download } = require("./controllers/download");
 const app = express();
 
 // Middlewares
-app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/", post_download);
+app.get("/:filename", get_download);
 
 app.use((req, res, next) => {
-  res.status(404).json({ message: "Invalid route", errors: true });
+  return res.status(404).json({ message: "Invalid route", errors: true });
 });
 
 app.listen(process.env.PORT, () => {
